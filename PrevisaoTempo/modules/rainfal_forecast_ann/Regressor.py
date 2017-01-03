c54557792cac547db892db0975d5108256ce1253
Nicoli Araujo <npa.matsu@gmail.com>
2017-01-03 12:24:50 -0400

    Resultados organizados

--- PrevisaoTempo/modules/rainfal_forecast_ann/Regressor.py
+++ PrevisaoTempo/modules/rainfal_forecast_ann/Regressor.py
@@ -103,66 +103,7 @@ class ResultNet():
         
 class RainfallRegressor(object):
     '''
-    Treinar, testar e validar um conjunto de dados de 1950 a 2015 com as anomalias
-    dos dados mensais de precipitação, tsa, e ninos. O output deve ser o volume de
-    cada mês, e o input são os dados de uma quantidade time_gap de meses antes do
-    mês avaliado.
-
-    1) ler dataset
-    2) identificar inputs
-    3) identificar output
-    4) setar rede:
-        tipo: time series
-        medida de performance: mse
-        passar input
-        passar output
-
-        net.trainParam.epochs = 100;
-        net.trainParam.mu = 0.003;
-        net.trainParam.mu_dec = 0.01;
-        net.trainParam.mu_inc = 7;
-        net.trainParam.mu_max = 10^10;
-
-
-    5)dividir:
-        função de treinamento: divideblock
-        quantidades de dados que irão para treinamento, validação e teste
-    6) setar parâmetros de treinamentos:
-
-    7)qtdLayers = 10;
-    netLayers = (1:qtdLayers);
-
-    mseVCT = zeros(qtdLayers:1);
-    mapeVCT = zeros(qtdLayers:1);
-
-    for i = 1:qtdLayers
-        clear train;
-        net.layers{1}.dimensions = netLayers(i);
-        net = configure(net,trainData,target);
-        [net,tr] = train(net, trainData, target);
-
-        %getting performance parameters
-        %getting Mean Square Error
-        result = net(trainData);
-        error = result - target;
-        mseVCT(i,1) = mse(error);
-
-        %getting Mean Absolute Percentage Error (MAPE)
-        [nRowsTD,nCollumnsTD] = size(tr.testInd);
-        errorTestData = zeros(2,nRowsTD);
-        for j = 1:nRowsTD
-            indice = tr.testInd(:,j);
-            errorTestData(:,j) = result(:,indice) - target(:,indice);
-        end
-        mapeVCT(i,1) = 100*(mae(errorTestData));
-    end
-
-
-    %display perform parameters
-    display(mapeVCT);
-    %display((100 - mapeVCT))
-    display(mseVCT);
-
+    
     '''
 
     def read_data_set(self, filename):
