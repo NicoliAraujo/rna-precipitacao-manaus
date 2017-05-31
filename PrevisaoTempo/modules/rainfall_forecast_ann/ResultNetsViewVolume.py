898cc72f66a59be0f53af4e327e05348efd9462d
Nicoli Araujo <npa.matsu@gmail.com>
2017-05-31 09:18:29 -0400

    Acuracia Classificação

--- PrevisaoTempo/modules/rainfall_forecast_ann/ResultNetsViewVolume.py
+++ PrevisaoTempo/modules/rainfall_forecast_ann/ResultNetsViewVolume.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 '''
-Created on 31 de out de 2016
+Created on 10 de mai de 2017
 
 @author: pibic-elloa-nicoli
 '''
+
 
 import os
 
@@ -25,24 +26,30 @@ class ResultNetsView():
         '''
         
         dict_net = {'Arquitetura': [], 'FA': [], 'Alpha': [],
-                'TA': [], 'FV': [], 'MSE': [], 'MAE':[]}
+                'TA': [], 'FV': [],'ACURACIA': []}
         for result_net in self.result_net_list:
             dict_net['Arquitetura'].append(str(result_net.net.hidden_layer_sizes))
             dict_net['FA'].append(result_net.net.activation)
             dict_net['Alpha'].append(result_net.net.alpha)
             dict_net['TA'].append(result_net.net.learning_rate_init)
             dict_net['FV'].append(result_net.net.validation_fraction)
-            dict_net['MSE'].append(round(result_net.mse, 5))
-            dict_net['MAE'].append(round(result_net.mae, 5))
+            #dict_net['MSE'].append(round(result_net.mse, 5))
+            #dict_net['MAE'].append(round(result_net.mae, 5))
+            dict_net['ACURACIA'].append(round(result_net.acuracia, 5))
         self.df = pd.DataFrame(dict_net)
-        self.df.sort_values(by='MSE', ascending=True, inplace=True)
+        self.df.sort_values(by='ACURACIA', ascending=False,inplace=True)
         self.df.index = [i for i in range(1, len(self.df) + 1)]
-        cols = ['Arquitetura', 'FA', 'Alpha', 'TA', 'FV', 'MSE', 'MAE']
+        cols = ['Arquitetura', 'FA', 'Alpha', 'TA', 'FV', 'ACURACIA']
         self.df = self.df[cols]
-
+        #print(self.df)
+        #print(self.df.to_latex())
     def save_results(self, filename):
-        with open(filename, 'w') as file:
-            self.df.to_csv(filename)
+        os.makedirs(filename[0], exist_ok=True)
+        name = filename[0]+filename[1]
+        with open(name, 'w') as file:
+            self.df.to_csv(name)
+        with open(name[:-4]+'.tex', 'w') as file:
+            file.write(self.df.to_latex())
 
 
 class ResultsParser():
@@ -61,22 +68,17 @@ class ResultsParser():
     def read_data_set(self, filename):
         return pd.read_csv(filename, sep=r',', index_col=0)
     
-    def __init__(self, result_nets_list, test_data, month, num_nets, source_df_filename):
+    def __init__(self, result_nets_list, test_data, month, num_nets):
         self.result_nets_list = result_nets_list
         self.test_data = test_data
         self.month = month
         
-        self.source_df_sum = self.read_data_set(source_df_filename).sum()
         
         self.parser_results_anomaly_df_seaborn = pd.DataFrame()
-        self.parser_results_df_seaborn = pd.DataFrame()
-        self.parser_results_anomaly_volume_df_seaborn = pd.DataFrame()
-        self.parser_results_df_volume_seaborn = pd.DataFrame()        
+        self.parser_results_df_seaborn = pd.DataFrame()       
         
         self.results_df = pd.DataFrame()
-        self.results_df_volume = pd.DataFrame()
         self.results_anomaly_df = pd.DataFrame()
-        self.results_anomaly_volume_df = pd.DataFrame()
         
         self.num_nets = num_nets
         
@@ -85,24 +87,23 @@ class ResultsParser():
         '''parser do df dos resultados previstos x o encontrado pro seaborn'''
         self.parser_results_df_seaborn = pd.DataFrame(self.test_data['output'])
         self.parser_results_df_seaborn.index = [i for i in range(len(self.test_data['output']))]
-        self.parser_results_df_seaborn.rename(columns={'rainfall_' + str(self.month):'Norm'}, inplace=True)
+        self.parser_results_df_seaborn.rename(columns={'rainfall_' + str(self.month):'Volume'}, inplace=True)
         self.parser_results_df_seaborn['Year'] = pd.Series(self.test_data['output'].index)
         self.parser_results_df_seaborn['from'] = 'y'
-    
         for i in range(self.num_nets):
-            results_predict = pd.DataFrame(self.result_nets_list[i].net.predict(self.test_data['input']))
-            results_predict.rename(columns={0:'Norm'}, inplace=True)
+            results_predict = pd.DataFrame(self.result_nets_list[i].result_data_total)
+            results_predict.rename(columns={0:'Volume'}, inplace=True)
             results_predict['Year'] = pd.Series(self.test_data['output'].index)
             results_predict['from'] = 'net_' + str(i)
             self.parser_results_df_seaborn = self.parser_results_df_seaborn.append(results_predict)
-    
+        self.parser_results_df_seaborn.index = [i for i in range(1, len(self.parser_results_df_seaborn)+1)]
+        print('\nresultsdfseaborn',self.parser_results_df_seaborn)
     def set_seaborn_dfs(self):
         '''datasets para seaborn'''
         self.set_parser_results_seaborn()
-        self.parser_results_anomaly_df_seaborn = self.from_abs_to_anomaly(self.parser_results_df_seaborn, 'Norm')
-        self.parser_results_df_volume_seaborn = self.from_norm_to_volume(self.parser_results_df_seaborn)
-        self.parser_results_anomaly_volume_df_seaborn = self.from_norm_to_volume_seaborn(self.parser_results_anomaly_df_seaborn)
-        
+        self.parser_results_anomaly_df_seaborn = self.from_abs_to_anomaly(self.parser_results_df_seaborn, 'Volume')
+        print('\nresults anomalia pro seaborn', self.parser_results_anomaly_df_seaborn)
+
     def save_df(self, filename, df):
         os.makedirs(filename[0], exist_ok=True)
         with open(filename[0] + filename[1], 'w') as file:
@@ -112,38 +113,25 @@ class ResultsParser():
         '''Datasets para visualização'''
         self.results_df['y'] = self.test_data['output']
         for i in range(self.num_nets):
-            self.results_df['net_' + str(i)] = self.result_nets_list[i].net.predict(self.test_data['input'])
+            self.results_df['net_' + str(i)] = self.result_nets_list[i].result_data_total
             self.results_df.index = self.test_data['output'].index
- 
+        #print('resultsdf',self.results_df)
         self.results_anomaly_df = self.from_abs_to_anomaly(self.results_df)
-        
-        self.results_df_volume = self.from_norm_to_volume(self.results_df)
-        self.results_anomaly_volume_df = self.from_norm_to_volume(self.results_anomaly_df)
-        
-    def from_norm_to_volume(self, df_norm):
-        '''retorna um df com resultados em volume'''
-        return df_norm * self.source_df_sum['rainfall_' + self.month]
-    
-    def from_abs_to_anomaly(self, abs_df, attr=None):
+        #print('results anomalia', self.results_anomaly_df)
+
+    def from_abs_to_anomaly(self, df, attr=None):
         if attr == None:
-            attr = abs_df.columns
-        anomaly_df = abs_df.copy()
+            attr = df.columns
+        anomaly_df = df.copy()
         anomaly_df[attr] -= self.results_df['y'].mean()
         return anomaly_df
+
     
-    def from_norm_to_volume_seaborn(self, df_norm):
-        df_volume = df_norm.copy()
-        df_volume['Norm'] = self.from_norm_to_volume(df_norm)
-        return df_volume
-    
-    def save_results_dfs(self, filename_norm_results_view, filename_norm_results_anomaly_view, filename_results_df_volume_view, filename_results_anomaly_volume_view):
+    def save_results_dfs(self, filename_norm_results_view, filename_norm_results_anomaly_view):
         self.save_df(filename_norm_results_view, self.results_df)
         self.save_df(filename_norm_results_anomaly_view, self.results_anomaly_df)
-        self.save_df(filename_results_df_volume_view, self.results_df_volume)
-        self.save_df(filename_results_anomaly_volume_view, self.results_anomaly_volume_df)
+
         
-    def save_seaborn_dfs(self, filename_results_df_volume_seaborn, filename_anomaly_volume_seaborn, filename_results_norm_seaborn, filename_norm_anomaly_seaborn):
-        self.save_df(filename_results_df_volume_seaborn, self.parser_results_df_volume_seaborn)
-        self.save_df(filename_anomaly_volume_seaborn, self.parser_results_anomaly_volume_df_seaborn)
+    def save_seaborn_dfs(self, filename_results_norm_seaborn, filename_norm_anomaly_seaborn):
         self.save_df(filename_results_norm_seaborn, self.parser_results_df_seaborn)
         self.save_df(filename_norm_anomaly_seaborn, self.parser_results_anomaly_df_seaborn)
--- PrevisaoTempo/modules/rainfall_forecast_ann/main.py
+++ PrevisaoTempo/modules/rainfall_forecast_ann/main.py
@@ -3,53 +3,48 @@ Created on 26 de fev de 2017
 
 @author: nicoli
 '''
-#from modules.rainfal_forecast_ann.Regressor import R
+# from modules.rainfal_forecast_ann.Regressor import R
 
-from modules.rainfall_forecast_ann.RainfallRegressorNets import RainfallRegressorNets
-from modules.rainfall_forecast_ann.ResultsParser import ResultsParser
+from Classifier import RainfallRegressorNets
+#from RainfallRegressorNets import RainfallRegressorNets
+from ResultNetsViewVolume import ResultNetsView, ResultsParser
+import pandas as pd
 
+
+def read_data_set(filename):
+        return pd.read_csv(filename, sep=r',', index_col=0)
 
 if __name__ == '__main__':
     MONTH = '01'
     TIME_GAP = '6'
     EXTENSION = 'csv'
-    FILENAME = '../../data/files/anninputs/normalizedinputs/' + MONTH+ '_' + TIME_GAP + '.' + EXTENSION
+    FILENAME = '../../data/files/anninputs/nonnormalizedinputs/' + MONTH + '_' + TIME_GAP + '.' + EXTENSION
+    FILENAME_NORM_ANOMALY_VIEW = ['../../data/files/ann_output_files/scale/ann_output_view/', MONTH + '_' + TIME_GAP + '_regression_stdscale_anomaly.csv']
+    #FILENAME_NORM_ANOMALY_SEABORN = ['../../data/files/ann_output_files/scale/ann_output_seaborn/', MONTH + '_' + TIME_GAP + '_regression_stdscale_anomaly_seaborn.csv']
+    
+    FILENAME_PREDICT = ['../../data/files/ann_output_files/scale/ann_output_view/' , MONTH + '_' + TIME_GAP + '_regression_stdscale.csv']
+    #FILENAME_PREDICT_SEABORN = ['../../data/files/ann_output_files/scale/ann_output_seaborn/', MONTH + '_' + TIME_GAP + '_regression_stdscale_seaborn.csv']
     
     
-    FILENAME_NORM_ANOMALY_VIEW = ['../../data/files/ann_output_files/ann_output_view/', MONTH + '_' + TIME_GAP + '_regression_normalized_anomaly.csv']
-    FILENAME_NORM_ANOMALY_SEABORN = ['../../data/files/ann_output_files/ann_output_seaborn/', MONTH + '_' + TIME_GAP + '_regression_normalized_anomaly_seaborn.csv']
-    
-    FILENAME_PREDICT = ['../../data/files/ann_output_files/ann_output_view/' , MONTH + '_' + TIME_GAP + '_regression_normalized.csv']
-    FILENAME_PREDICT_SEABORN = ['../../data/files/ann_output_files/ann_output_seaborn/', MONTH + '_' + TIME_GAP + '_regression_normalized_seaborn.csv']
-    
-    FILENAME_VOLUME_SEABORN = ['../../data/files/ann_output_files/ann_output_seaborn/', MONTH + '_' + TIME_GAP + '_regression_volume_seaborn.csv']
-    FILENAME_VOLUME = ['../../data/files/ann_output_files/ann_output_view/', MONTH + '_' + TIME_GAP + '_regression_volume.csv']
- 
-    FILENAME_SOURCE = '../../data/files/original/csv/AllData.csv'
      
-    FILENAME_VOLUME_ANOMALY_VIEW = ['../../data/files/ann_output_files/ann_output_view/', MONTH + '_' + TIME_GAP + '_regression_volume_anomaly.csv']
-    FILENAME_VOLUME_ANOMALY_SEABORN = ['../../data/files/ann_output_files/ann_output_seaborn/', MONTH + '_' + TIME_GAP + '_regression_volume_anomaly_seaborn.csv']
-     
-    RESULT_NETS_CSV = ['../../data/files/ann_output_files/', MONTH+ '_' + TIME_GAP + '_regression_dataset_normalized.csv']
+    RESULT_NETS_CSV = ['../../data/files/ann_output_files/scale/', MONTH + '_' + TIME_GAP + '_regression_dataset_stdscale.csv']
     
+    # normalizando
+    dataset_non_normalized = read_data_set(FILENAME)
+    #print(dataset_non_normalized.mean())
     
-    RFANN = RainfallRegressorNets(FILENAME, n_layers=2, n_nodes=7)
-    RFANN.train_test_nets()
-    RFANN.save_networks(MONTH, TIME_GAP)
+    rfann = RainfallRegressorNets(dataset_non_normalized)
+    rfann.train_test_nets()
+    rfann.save_networks(MONTH, TIME_GAP)
     
-    PS = ResultsParser(RFANN.result_networks, RFANN.test_data, MONTH, num_nets=3, source_df_filename=FILENAME_SOURCE)
+    PS = ResultsParser(rfann.neural_networks, rfann.test_data, MONTH, 5)
+    
     PS.set_results_dfs()
-    PS.set_parser_results_seaborn()
+    #PS.set_seaborn_dfs()
  
     PS.save_results_dfs(filename_norm_results_view=FILENAME_PREDICT,
-                        filename_norm_results_anomaly_view=FILENAME_NORM_ANOMALY_VIEW, 
-                        filename_results_df_volume_view=FILENAME_VOLUME, 
-                        filename_results_anomaly_volume_view=FILENAME_VOLUME_ANOMALY_VIEW)
+                        filename_norm_results_anomaly_view=FILENAME_NORM_ANOMALY_VIEW)
+                        
     
-    PS.save_seaborn_dfs(filename_results_df_volume_seaborn=FILENAME_VOLUME_SEABORN,
-                        filename_anomaly_volume_seaborn=FILENAME_VOLUME_ANOMALY_SEABORN, 
-                        filename_results_norm_seaborn=FILENAME_PREDICT_SEABORN, 
-                        filename_norm_anomaly_seaborn=FILENAME_NORM_ANOMALY_SEABORN)
-    
-    
-    
+    #PS.save_seaborn_dfs(filename_results_norm_seaborn=FILENAME_PREDICT_SEABORN,
+    #                    filename_norm_anomaly_seaborn=FILENAME_NORM_ANOMALY_SEABORN)
